b373dd13dc2af8f071df570c65196db4d1129be8
GinoSahagun <sahagun.gino@gmail.com>
2023-03-03 15:35:49 -0800

    main branch

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -3,6 +3,21 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
@@ -11,6 +26,12 @@ const nextConfig = {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    // images: {
+    //   loader: 'imgix',
+    //   path: 'the "domain" of your Imigix source',
+    // },
   },
 };
 
